41f04c01c07f34aba34a0c4f02a42f5d03badd5a
ky0422 <ky0422a@gmail.com>
2023-07-02 22:44:58 +0900

    `update`: updated CPU

---
+++ src/cpu.rs
@@ -0,0 +1,37 @@
+use crate::{memory::Memory, registers::Registers};
+
+#[doc=include_str!("../README.md")]
+#[derive(Debug, Default)]
+pub struct Cpu {
+    pub registers: Registers,
+    pub memory: Memory,
+}
+
+impl Cpu {
+    pub fn new(memory: Memory) -> Cpu {
+        Cpu {
+            registers: Registers::default(),
+            memory,
+        }
+    }
+
+    pub fn execute(&mut self) {
+        loop {
+            let opcode = self.memory.read(self.registers.pc);
+            self.registers.pc += 1;
+
+            if self.execute_instruction(opcode) {
+                break;
+            }
+        }
+    }
+
+    fn execute_instruction(&mut self, opcode: u8) -> bool /* Break */ {
+        match opcode {
+            0x00 => return true,
+            _ => {}
+        }
+
+        false
+    }
+}
