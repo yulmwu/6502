18803f8745a55c46415864f513a34ba02a39f83a
ky0422 <ky0422a@gmail.com>
2023-07-02 22:45:06 +0900

    `update`: updated `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use crate::{memory::Memory, cpu::Cpu};
+
+mod cpu;
+mod memory;
+mod registers;
+
 fn main() {
+    let memory = Memory::default();
+    let mut cpu = Cpu::new(memory);
+
+    cpu.execute();
+
     println!("Hello, world!");
 }
