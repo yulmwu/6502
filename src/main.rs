ff253c86d344460d81fea25dd1ea0217d680d279
ky0422 <ky0422a@gmail.com>
2023-07-08 00:19:54 +0900

    `update`: updated Emulator

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use emulator::{cpu::Cpu, memory::Memory};
+use emulator::{
+    cpu::Cpu,
+    memory::{Memory, MemoryBus},
+};
 
 fn main() {
     let memory = Memory::default();
     let mut cpu = Cpu::new(memory);
-    cpu.load(&[0x00]);
     cpu.reset();
+    cpu.registers.a = 0x01;
+    cpu.registers.set_flag_carry(true);
+    cpu.memory.write(0x8000, 0x69); // ADC, Immediate
+    cpu.memory.write(0x8001, 0xFE);
+    cpu.memory.write(0x8002, 0x00);
+
     cpu.execute();
 
-    println!("{:?}", cpu.memory[0]);
+    println!(
+        "{:?} {} {}",
+        cpu.registers.a,
+        cpu.registers.get_flag_carry(),
+        cpu.registers.get_flag_overflow()
+    );
 }
