2f68cb8bbec3d1b38e4688204161c8e86b772dd8
ky0422 <ky0422a@gmail.com>
2023-08-21 22:38:42 +0900

    `update`: updated Assembler

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -9,45 +9,105 @@ pub use parser::*;
 pub use tokenizer::*;
 
 use logos::Logos;
+use std::collections::HashMap;
 
-pub fn assemble(source: &str) -> Vec<u8> {
-    let lexer = TokenKind::lexer(source);
-    let mut parser = Parser::new(lexer);
-    let p = parser.parse();
-
-    let mut bytes = Vec::new();
-
-    for statement in p.0 {
-        match statement {
-            Statement::Instruction(instruction) => bytes.extend(assemble_instruction(instruction)),
-            Statement::Label(_) => todo!(),
-        }
-    }
-
-    bytes
+pub struct Assembler {
+    pub source: String,
+    pointer: usize,
+    labels: HashMap<String, u16>,
 }
 
-fn assemble_instruction(instruction: Instruction) -> Vec<u8> {
-    let Instruction {
-        opcode,
-        operand: Operand {
-            addressing_mode,
-            value,
-        },
-    } = instruction;
-    let mut bytes = vec![instruction_to_byte(opcode, addressing_mode)];
-
-    if let Some(value) = value {
-        match value {
-            OperandData::Number(number_type) => match number_type {
-                NumberType::Decimal8(value) => bytes.extend(value.to_be_bytes()),
-                NumberType::Decimal16(value) => bytes.extend(value.to_be_bytes()),
-                NumberType::Hexadecimal8(value) => bytes.extend(value.to_be_bytes()),
-                NumberType::Hexadecimal16(value) => bytes.extend(value.to_be_bytes()),
-            },
-            OperandData::Label(_) => todo!(),
+impl Assembler {
+    pub fn new(source: String) -> Self {
+        Self {
+            source,
+            pointer: 0,
+            labels: HashMap::new(),
         }
     }
 
-    bytes
+    pub fn assemble(&mut self) -> Vec<u8> {
+        let lexer = TokenKind::lexer(&self.source);
+        let mut parser = Parser::new(lexer);
+        let p = parser.parse();
+
+        let mut bytes = Vec::new();
+
+        for statement in p.0.clone() {
+            match statement {
+                Statement::Label(label) => self.assemble_label(label),
+                Statement::Instruction(instruction) => {
+                    self.pointer += 1;
+
+                    let Instruction {
+                        opcode: _,
+                        operand: Operand { value, .. },
+                    } = instruction;
+
+                    if let Some(value) = value {
+                        match value {
+                            OperandData::Number(number_type) => match number_type {
+                                NumberType::Decimal8(_) | NumberType::Hexadecimal8(_) => {
+                                    self.pointer += 1
+                                }
+                                NumberType::Decimal16(_) | NumberType::Hexadecimal16(_) => {
+                                    self.pointer += 2
+                                }
+                            },
+                            OperandData::Label(_) => self.pointer += 1,
+                        }
+                    }
+                }
+            }
+        }
+
+        self.pointer = 0;
+
+        for statement in p.0 {
+            if let Statement::Instruction(instruction) = statement {
+                let i = self.assemble_instruction(instruction);
+                println!("{:X?} {}", i, self.pointer);
+                bytes.extend(i)
+            }
+        }
+
+        println!("{:?}", self.labels);
+
+        bytes
+    }
+
+    fn assemble_instruction(&mut self, instruction: Instruction) -> Vec<u8> {
+        let Instruction {
+            opcode,
+            operand: Operand {
+                addressing_mode,
+                value,
+            },
+        } = instruction;
+        let mut bytes = vec![instruction_to_byte(opcode, addressing_mode)];
+
+        if let Some(value) = value {
+            match value {
+                OperandData::Number(number_type) => match number_type {
+                    NumberType::Decimal8(value) => bytes.extend(value.to_be_bytes()),
+                    NumberType::Decimal16(value) => bytes.extend(value.to_be_bytes()),
+                    NumberType::Hexadecimal8(value) => bytes.extend(value.to_be_bytes()),
+                    NumberType::Hexadecimal16(value) => bytes.extend(value.to_be_bytes()),
+                },
+                OperandData::Label(label) => {
+                    let label_address = self.labels.get(&label).unwrap();
+                    let relative_address: u8 =
+                        (*label_address as i16 - self.pointer as i16 - 2) as u8;
+                    bytes.extend(relative_address.to_be_bytes());
+                }
+            }
+        }
+
+        self.pointer += bytes.len();
+        bytes
+    }
+
+    fn assemble_label(&mut self, label: String) {
+        self.labels.insert(label, self.pointer as u16);
+    }
 }
