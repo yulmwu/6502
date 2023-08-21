a7ee2432292df7b79340cbc88395b6ade9766449
ky0422 <ky0422a@gmail.com>
2023-08-21 22:39:03 +0900

    `update`: updated Emulator

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use emulator::assemble;
+use emulator::{
+    cpu::Cpu,
+    memory::{memory_hexdump, Memory},
+    Assembler,
+};
 
 fn main() {
-    let src = r#"
-LDA #$FF
-LDA $FFFF
-LDA $FFFF,X
-LDA $FFFF,Y
-JMP ($FFFF)
-LDA ($FF, X)
-LDA ($FF), Y
-LDA $FF
-LDX $FF,Y
-LDY $FF,X
-CLC
-"#;
-    println!("{:?}", assemble(src));
+    let s = r#"
+LDA #$02
+CMP #$01
+BNE FOO
+LDA #$01
+STA $00
+BRK
+
+FOO:
+    LDA #$01
+    STA $01
+    BRK
+    "#;
+    let src = Assembler::new(s.to_string()).assemble();
+    println!("{:?}", src);
+    let mut emulator = Cpu::<Memory>::default();
+    emulator.reset();
+    emulator.load(&src);
+    emulator.execute();
+    println!("{}", memory_hexdump(&emulator.memory, 0x0000, 0x0020));
+    // println!("{}", memory_hexdump(&emulator.memory, 0x8000, 0x800F));
 }
